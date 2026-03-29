8d7d5bc0683d54b491dbb3da307d5d94f23789e4
Rahmaylah <sufianirehema29@gmail.com>
2026-03-29 10:18:48 +0300

    admin can manage user accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type', 'department', 'registration_number', 'course_id', 'mentor')
+    list_filter = ('user_type', 'department', 'course_id')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'registration_number')
+
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user', 'user_type', 'department')
+        }),
+        ('Student Fields', {
+            'fields': ('registration_number', 'course_id', 'mentor'),
+            'classes': ('collapse',),
+        }),
+        ('Mentor Fields', {
+            'fields': ('max_students', 'is_admin'),
+            'classes': ('collapse',),
+        }),
+    )
